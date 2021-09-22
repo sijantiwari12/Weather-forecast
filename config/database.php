25c03f26bf25931684e45873d3b572b11059f921
Sijan Tiwari <sijan.tiwari@selu.edu>
2021-09-21 20:19:42 -0500

    config and other required files setup

---
+++ config/database.php
@@ -0,0 +1,24 @@
+<?php
+if (!defined('SERVERNAME')) {
+    define('SERVERNAME', '127.0.0.1');
+}
+if (!defined('USERNAME')) {
+    define('USERNAME','root');
+}
+if (!defined('PASSWORD')) {
+    define('PASSWORD','');
+}
+if (!defined('DB')) {
+    define('DB','apitoken');
+}
+
+
+
+
+
+
+
+
+
+
+
